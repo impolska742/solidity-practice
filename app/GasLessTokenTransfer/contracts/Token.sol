d00bd9464c173e2a6610725624f1bc83c15903dd
impolska742 <vaibhav19bhardwaj@gmail.com>
2023-08-16 03:14:30 +0530

    Added changes

--- app/GasLessTokenTransfer/contracts/Token.sol
+++ app/GasLessTokenTransfer/contracts/Token.sol
@@ -6,11 +6,27 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract Token is ERC20, Ownable, ERC20Permit {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
-        _mint(msg.sender, 1000 * 10 ** decimals());
-    }
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function gaslessTransfer(
+        address sender,
+        address receiver,
+        uint amount,
+        uint fee,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        // Permit - sender approves this contract to spend amount + fee
+        // transferFrom(sender, receiver, amount)
+        // transferFrom(sender, msg.sender, fee)
+        permit(sender, address(this), amount + fee, deadline, v, r, s);
+        transferFrom(sender, receiver, amount);
+        transferFrom(sender, msg.sender, fee);
     }
 }
--- app/GasLessTokenTransfer/test/ERC20Permit.ts
+++ app/GasLessTokenTransfer/test/ERC20Permit.ts
@@ -1,7 +1,6 @@
 import { ethers } from "hardhat";
 import { BigNumberish, constants, Signature, Wallet } from 'ethers'
-import { splitSignature } from 'ethers/lib/utils'
-
+import { expect } from "chai";
 
 export async function getPermitSignature(
   wallet: Wallet,
@@ -16,8 +15,6 @@ export async function getPermitSignature(
     '1',
     wallet.getChainId(),
   ])
-
-  console.log({nonce, name, version, chainId})
 
   const domain = {
     name,
@@ -59,20 +56,9 @@ export async function getPermitSignature(
     deadline,
   }
 
-  console.log({
-    domain,
-    types,
-    valueToSign
-    
-  })
-
-  return splitSignature(
-    await wallet._signTypedData(
-      domain,
-      types,
-      valueToSign
-    )
-  )
+  const result = await wallet._signTypedData(domain, types, valueToSign);
+  const signature = ethers.utils.splitSignature(result);
+  return signature
 }
 
 describe("ERC20Permit", function () {
@@ -87,17 +73,18 @@ describe("ERC20Permit", function () {
 
         await token.deployed()
 
-        const GasLessTokenTransfer = await ethers.getContractFactory("GasLessTokenTransfer");
-        const gaslessTokenTransfer = await GasLessTokenTransfer.deploy(token.address);
-
         await token.mint(signer.address, 10000);
         const amount = 1000;
         const fee = 50;
 
+        expect((await token.balanceOf(signer.address))).to.equal(ethers.BigNumber.from(10000));
+
         const deadLine = ethers.constants.MaxUint256;
 
-        const {v, r, s} = await getPermitSignature(signer as unknown as Wallet, token, gaslessTokenTransfer.address, amount, deadLine)
+        const {v, r, s} = await getPermitSignature(signer as unknown as Wallet, token, token.address, amount, deadLine)
 
-        await gaslessTokenTransfer.send(signer.address, reciever.address, amount, fee, deadLine, v, r, s);
+        console.log({v, r, s});
+
+        await token.gaslessTransfer(signer.address, reciever.address, amount, fee, deadLine, v, r, s);
     });
 })
