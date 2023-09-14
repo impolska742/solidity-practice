5e7fb6545530b4c8d96b1f526e17be7e5957cf01
impolska742 <vaibhav19bhardwaj@gmail.com>
2023-09-14 13:06:17 +0530

    feat: deployContract

---
+++ simple-storage/src/Counter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+contract Counter {
+    uint256 public count = 0;
+
+    function increment() public {
+        count += 1;
+    }
+
+    function decrement() public {
+        count += 1;
+    }
+}
