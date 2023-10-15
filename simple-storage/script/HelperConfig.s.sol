da7e9b0ed6f2bf64cfb8d202710354daf43db698
impolska742 <vaibhav19bhardwaj@gmail.com>
2023-10-15 20:56:25 +0530

    feat: added mock aggregator contract for local testing

--- simple-storage/script/HelperConfig.s.sol
+++ simple-storage/script/HelperConfig.s.sol
@@ -7,6 +7,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     // If we are on a local anvil chain, we deploy mocks
@@ -14,6 +15,9 @@ contract HelperConfig is Script {
     struct NetworkConfig {
         address priceFeed;
     }
+
+    uint8 public constant DECIMALS = 18;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     NetworkConfig public activeNetworkConfig;
 
@@ -25,30 +29,49 @@ contract HelperConfig is Script {
         } else if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
     function getGoerliConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory GoerliNetworkConfig = NetworkConfig({
+        NetworkConfig memory goerliConfig = NetworkConfig({
             priceFeed: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
         });
-        return GoerliNetworkConfig;
+        return goerliConfig;
     }
 
     function getSepoliaConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory GoerliNetworkConfig = NetworkConfig({
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
-        return GoerliNetworkConfig;
+        return sepoliaConfig;
     }
 
     function getMainnetConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory GoerliNetworkConfig = NetworkConfig({
+        NetworkConfig memory mainnetConfig = NetworkConfig({
             priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
         });
-        return GoerliNetworkConfig;
+        return mainnetConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        // 1. Deploy mock contract
+        // 2. Return mock contract address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockV3Aggregator = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockV3Aggregator)
+        });
+        return anvilConfig;
+    }
 }
