3490d183c69acc6428b93f893daaa8c16e18d88d
impolska742 <vaibhav19bhardwaj@gmail.com>
2023-12-06 00:13:28 +0530

    feat: added testcases on basicnft.sol

--- app/erc721/test/BasicNft.t.sol
+++ app/erc721/test/BasicNft.t.sol
@@ -3,7 +3,30 @@ pragma solidity 0.8.21;
 
 import {Test} from "forge-std/Test.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 
-contract TestBasicNft is Test {
-   
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public USER = makeAddr("USER");
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public {
+        string memory expectedName = "Dogie";
+        assertEq(bytes(expectedName), bytes(basicNft.name()));
+    }
+    
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG_URI);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(basicNft.tokenURI(0))) == keccak256(abi.encodePacked(PUG_URI)));
+    }
 }
