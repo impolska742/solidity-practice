a37ae589b48a94d3279f14234c9f9c868f5c271f
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2024-09-25 01:35:48 +0530

    feat: DecentralizedStableCoin.sol initialized with mint & burn

--- app/stablecoin/src/DecentralizedStableCoin.sol
+++ app/stablecoin/src/DecentralizedStableCoin.sol
@@ -1,6 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-contract DecentralizedStableCoin {
-    
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title Decentralized Stable Coin
+ * @dev A decentralized stable coin contract
+ * @author Vaibhav Bhardwaj
+ * Collateral: Exogenous (wETH & wBTC)
+ * Minting: Algorithmic
+ * 
+ * This contract is just an ERC20 implementation for our stable coin. It is meant to be governed by our DSCEngine.
+ * 
+ */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor() ERC20("Decentralized Stable Coin", "DSC") Ownable(msg.sender) {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero(); 
+        }
+        if (_amount > balance) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        _burn(msg.sender, _amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
